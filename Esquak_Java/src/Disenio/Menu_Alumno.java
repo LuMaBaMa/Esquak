d7ffcc312b93a089f922803d185d66056058812d
LuMaBaMa <lbarriosm2100@alumno.ipn.mx>
2025-05-12 15:54:04 -0600

    Disenio 1 alumnos

--- Esquak_Java/src/Disenio/Menu_Alumno.java
+++ Esquak_Java/src/Disenio/Menu_Alumno.java
@@ -120,18 +120,25 @@ public class Menu_Alumno extends javax.swing.JFrame {
         });
 
         Asesorias.setBackground(new java.awt.Color(255, 255, 255));
+        Asesorias.setForeground(new java.awt.Color(0, 0, 0));
         Asesorias.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Materia", "Fecha", "Asesor"
             }
-        ));
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
         jScrollPane1.setViewportView(Asesorias);
+        Asesorias.getAccessibleContext().setAccessibleParent(jScrollBar1);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
