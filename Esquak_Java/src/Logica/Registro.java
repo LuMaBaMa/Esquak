d1a7b56c08190b5f60e6f87f8c15a06569b1e1a8
LuMaBaMa <lbarriosm2100@alumno.ipn.mx>
2025-05-12 14:44:39 -0600

    Prueba NetBeans

--- Esquak_Java/src/Logica/Registro.java
+++ Esquak_Java/src/Logica/Registro.java
@@ -4,7 +4,6 @@ import javax.swing.JTextField;
 public class Registro {
     public static void reg_alumno(JTextField Boleta_Alumno, JTextField Nombre_Alumno, JTextField Contrasenia_Alumno){
         String sql = "insert into alumnos(alu_boleta, alu_nombre, alu_contrasenia) values (?,?,?)";
-        Statement stmt;
         int resultado;
         Connection con = null;
         Coneccion conx = new Coneccion();
@@ -29,8 +28,8 @@ public class Registro {
     public static void reg_asesor(JTextField Codigo_Asesor, JTextField Nombre_Asesor,
             JTextField Contrasenia_Asesor, JTextField Materia_Asesor,
             JTextField Telefono_Asesor, JTextField Correo_Asesor){
-        String sql = "insert into asesores(id_asesor, as_nombre, as_contrasenia, as_materia, as_correo, as_telefono) values (?,?,?,?,?,?)";
-        Statement stmt;
+        String sql = "insert into asesores(id_asesor, as_nombre, as_contrasenia, as_materia, as_correo, as_telefono) "
+                + "values (?,?,?,?,?,?)";
         Coneccion conx = new Coneccion();
         Connection con = null;
         con = conx.conectar();
@@ -42,8 +41,8 @@ public class Registro {
             ps.setString(2,Nombre_Asesor.getText());
             ps.setString(3,Contrasenia_Asesor.getText());
             ps.setString(4,Materia_Asesor.getText());
-            ps.setString(5,Telefono_Asesor.getText());
-            ps.setString(6,Correo_Asesor.getText());
+            ps.setString(5,Correo_Asesor.getText());
+            ps.setString(6,Telefono_Asesor.getText());
             resultado = ps.executeUpdate();
             if(resultado == 1){
                 Disenio.Menu_Asesor menuas = new Disenio.Menu_Asesor();
